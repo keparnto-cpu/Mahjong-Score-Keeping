4b95675480e873f4a288af700e2aa3353840c3e9
keparnto-cpu <keparnto@mac.com>
2026-04-09 18:09:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const DATA_FILE = path.join(__dirname, "data", "scores.json");
 
 app.use(cors());
